d369b621d15891b4cd7f770eab6d35786eef77f8
jerome <jvgonzalvo@up.edu.ph>
2017-10-12 10:19:01 +0800

    Revert "Initial commit"
    This reverts commit 81d90d816b767ac0453b66ff6fcc730e25ce6908.

--- app/src/main/java/gonzalvo/dpsm/cas/upm/edu/ph/mem2speech/HandwrittenTextSourceFragment.java
+++
@@ -1,7 +0,0 @@
-package gonzalvo.dpsm.cas.upm.edu.ph.mem2speech;
-
-import android.graphics.Bitmap;
-
-public interface HandwrittenTextSourceFragment {
-    Bitmap getImage();
-}
--- app/src/main/java/gonzalvo/dpsm/cas/upm/edu/ph/mem2speech/recognizer/Recognizer.java
+++
@@ -1,7 +0,0 @@
-package gonzalvo.dpsm.cas.upm.edu.ph.mem2speech.recognizer;
-
-import android.graphics.Bitmap;
-
-public interface Recognizer {
-    String recognizeHandwritingFrom(Bitmap bitmap);
-}
